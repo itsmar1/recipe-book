6d4f16ee9124ee00e68d0605990db67d1efb8fdb
Marouane Ahal <d144013152@gmail.com>
2022-08-27 15:04:55 +0100

    effects & error handling

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -1,12 +1,42 @@
-import {Actions, ofType} from "@ngrx/effects";
+import {Actions, Effect, ofType} from "@ngrx/effects";
+import {map, switchMap} from 'rxjs/operators';
+import { environment } from '../../../environments/environment';
 
 import * as AuthActions from './auth.actions';
+import {HttpClient} from "@angular/common/http";
+import {catchError, of} from "rxjs";
+
+
+export interface AuthResponseData {
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
+}
+
+
 
 export class AuthEffects {
+  @Effect()
   authLogin = this.actions$.pipe(
-    ofType(AuthActions.LOGIN_START)
+    ofType(AuthActions.LOGIN_START),
+    switchMap((authData: AuthActions.LoginStart) => {
+      return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key='
+        + environment.firebaseAPIKey, {
+          email: authData.payload.email,
+          password: authData.payload.password,
+          returnSecureToken: true
+        }).pipe(catchError(error => {
+          // ...
+        of();
+      }), map(resData => {
+        of();
+      }));
+    })
   );
 
-  constructor(private actions$: Actions) {
+  constructor(private actions$: Actions, private http: HttpClient) {
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  firebaseAPIKey: 'AIzaSyCfUWXH_Ufky7KNkGS9Xs5lN2HIEhGPDKA',
 };
 
 /*
