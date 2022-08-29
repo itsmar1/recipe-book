6e8c8a2b881d8b4154222df0a41f62c5bdd6bc6d
Marouane Ahal <d144013152@gmail.com>
2022-08-29 02:47:51 +0100

    adding auto logout

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -8,6 +8,7 @@ import {catchError, of, throwError} from "rxjs";
 import {Injectable} from "@angular/core";
 import {Router} from "@angular/router";
 import {User} from "../user.model";
+import {AuthService} from "../auth.service";
 
 
 export interface AuthResponseData {
@@ -67,6 +68,9 @@ export class AuthEffects {
           returnSecureToken: true
         })
         .pipe(
+          tap(resData => {
+            this.authService.setLogoutTimer(+resData.expiresIn * 1000);
+          }),
           map(resData => {
             return handleAuthentication(
               +resData.expiresIn,
@@ -94,6 +98,9 @@ export class AuthEffects {
         password: authData.payload.password,
         returnSecureToken: true
       }).pipe(
+        tap(resData => {
+          this.authService.setLogoutTimer(+resData.expiresIn * 1000);
+        }),
         map(resData => {
           return handleAuthentication(
             +resData.expiresIn,
@@ -112,7 +119,7 @@ export class AuthEffects {
 
   @Effect({dispatch: false})
   authRedirect = this.actions$.pipe(
-    ofType(AuthActions.AUTHENTICATE_SUCCESS, AuthActions.LOGOUT),
+    ofType(AuthActions.AUTHENTICATE_SUCCESS),
     tap(() => {
       this.router.navigate(['/']);
     })
@@ -142,6 +149,8 @@ export class AuthEffects {
 
       if (loadedUser.token) {
         // this.user.next(loadedUser);
+        const expirationDate = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
+        this.authService.setLogoutTimer(expirationDate);
         return new AuthActions.AuthenticateSuccess({
           email: loadedUser.email,
           userId: loadedUser.id,
@@ -162,11 +171,17 @@ export class AuthEffects {
   authLogout = this.actions$.pipe(
     ofType(AuthActions.LOGOUT),
     tap(() => {
+      this.authService.clearLogoutTimer();
       localStorage.removeItem('userData');
+      this.router.navigate(['/auth']);
     })
   );
 
-  constructor(private actions$: Actions, private http: HttpClient, private router: Router) {
+  constructor(
+    private actions$: Actions,
+    private http: HttpClient,
+    private router: Router,
+    private authService: AuthService) {
   }
 }
 
